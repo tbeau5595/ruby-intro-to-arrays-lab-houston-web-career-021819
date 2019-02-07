2c9963731e9198b3b080ef8116d14f378128420b
Trey <tbeau5595@hotmail.com>
2019-02-07 01:24:47 +0000

    Automatically backed up by Learn

--- lib/intro_to_arrays.rb
+++ lib/intro_to_arrays.rb
@@ -6,3 +6,15 @@ def array_with_two_elements
   [cat, dog]
 end
 
+def first_element(array)
+  array[0]
+end
+
+def third_element(array)
+  array[2]
+end
+
+def last_element(array)
+  array[-1]
+end
+
